50c9d70f8279168e24878c033d083f614fbd10eb
Theo <teo.pvh@gmail.com>
2021-07-27 12:12:43 -0400

    add imagens e button subscribe

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
@@ -5,7 +6,7 @@ export function Header () {
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/" alt="IG.NEWS" />
+        <img src="/images/logo.svg" alt="ig.news" />
         <nav>
           <a className={styles.active}>Home</a>
           <a>Posts</a>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
 export default function Home() {
+  
   return (
     <>
       <Head>
@@ -17,9 +20,10 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.99 month</span>
           </p>
+          <SubscribeButton />
         </section>
-
-        <img src="/images/" alt="Girl coding" />
+        
+        <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
